8160293253fb4bfaaa971e53ab0317abcf9725f5
Álvaro Ferreira Pires de Paiva <alvarofepipa@gmail.com>
2018-08-31 19:52:11 -0300

    Bot starter

---
+++ src/ufrn/alvarofpp/player/CharacterType.java
@@ -0,0 +1,18 @@
+package ufrn.alvarofpp.player;
+
+/**
+ * player.CharacterType
+ *
+ * [description]
+ *
+ * @author Jim van Eeden - jim@riddles.io
+ */
+public enum CharacterType {
+    BIXIE,
+    BIXIETTE;
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase();
+    }
+}
